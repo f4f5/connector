d76e6918ef5deb7c726102eea9fb3656ba36028c
jazz <jazz.ada@qq.com>
2019-07-08 20:02:36 +0800

    dkjg

--- connector/node.py
+++ connector/node.py
@@ -13,7 +13,6 @@ import random
 import json
 import local
 from netio import ClientStream
-from netio import server
 
 
 class Node:
@@ -29,15 +28,18 @@ class Node:
         self.load_config()
         self.pingter = PyPing()
         self.ismainnode = False
-        self.client = ClientStream()
-        self.sever = server       
+        self.client = ClientStream(Node,Node)
 
     def load_config(self):
         serverConfig = {}
         with open('./config/server.json', encoding = 'utf8') as f:
             serverConfig = json.loads(f.read())
             self.server_type = serverConfig.get('server_type') 
-            self.ip = serverConfig.get('ip') 
+            self.ip = serverConfig.get('ip')
+            self.con_port = serverConfig.get('con_port')            
+            self.api_port = serverConfig.get('api_port')     
+            self.api_ip = self.ip + str(self.api_port)
+            self.con_ip = self.ip + str(self.con_port)
             self.location = serverConfig.get('location')            
             self.remain_connections = serverConfig.get('remain_connections')
             self.score = serverConfig.get('score')
@@ -46,8 +48,9 @@ class Node:
 
     def build_self_node(self):
         self.node = {}
-        self.node['ip'] = self.ip
-        self.node['remain_connections'] = self.remain_connection
+        self.node['con_ip'] = self.con_ip
+        self.node['api_ip'] = self.api_ip
+        self.node['remain_connections'] = self.remain_connections
         self.node['location'] = self.location
         self.node['score'] = self.score
         self.node['sever_type'] = self.server_type
@@ -73,14 +76,14 @@ class Node:
 
         if len(self.scores) < 100:            
             rs = await self.req_resource_file(random.choice(self.scores))  
-            self.node['father'] = self.ip          
+            self.node['father'] = self.con_ip          
             self.resource_mains = rs.resource_mains
             self.locations = rs.locations
             self.server_types = rs.server_types
             self.remain_connections = rs.remain_connections
             self.scores = rs.scores
             self.ismainnode = True   # indicate the server will be main node server.
-            util.opush(self.scores,self.ip)
+            util.opush(self.scores,self.con_ip)
             await self.notify({
                     'op': 'new_main_node',
                     'node': self.node
@@ -132,7 +135,7 @@ class Node:
 
     async def main_node_server(self, reader, writer):
         """
-        Server as a main node to receive and process message. This function can be thought
+        Server as a main node to receive and process data. This function can be thought
         to be controller in MVC moudel. 
 
         ---------
@@ -142,26 +145,32 @@ class Node:
         data = data.decode()
         data = json.loads(data)        
         addr = writer.get_extra_info('peername')
-        print(f"Received {message!r} from {addr!r}")
-        print(f"Send: {message!r}")
+        print(f"Received {data!r} from {addr!r}")
+        print(f"Send: {data!r}")
         resp = {
             'get_main_node':        self.resp_main_node,
-            'new_node':             self.resp_new_node,            'new_main_node':        self.resp_new_main_node,
+            'new_node':             self.resp_new_node,
+            'new_main_node':        self.resp_new_main_node,
             'get_resource_file':    self.resp_resource_file,  #just response the resource file
-            'delete_main_node':     self.resp_main_node_fail,    #just receive the info and do something
+            'delete_main_node':     self.data,    
             'delete_node':          self.resp_delete_node,
             'add_node':             self.resp_add_node,
-            'search_node':          self.search_node,
-            'update_node':          self.update_node,
+            # 'search_node':          self.search_node,
+            # 'update_node':          self.update_node,
             'heart_beat':           self.resp_heart_beat,
-            'set_main_node':        self.resp_set_main_node
+            'set_main_node':        self.resp_set_main_node,
+            
         }
-        resp.get(data['op'])(data, reader, writer)        
+        print('gonna request to main server: ',data['op'])
+        if data['op'] and resp.get(data['op']):
+            await resp.get(data['op'])(data, reader, writer) 
+        else:
+            print('request fail to main server: ',data['op'])       
         pass    
     
     async def node_server(self, reader, writer):
         """
-        Server as a normal node to receive and process message. This function can be thought 
+        Server as a normal node to receive and process data. This function can be thought 
         to be controller in MVC moudel.
 
         ---------
@@ -171,19 +180,23 @@ class Node:
         data = data.decode()
         data = json.loads(data)        
         addr = writer.get_extra_info('peername')
-        print(f"Received {message!r} from {addr!r}")
-        print(f"Send: {message!r}")
+        print(f"Received {data!r} from {addr!r}")
+        print(f"Send: {data!r}")
         resp = {
-            'get_main_node':        self.get_main_node,
-            'change_main_node':     self.change_main_node,
-            'delete_node':          self.delete_node,
-            'add_node':             self.add_node,
-            'search_node':          self.search_node,
-            'update_node':          self.update_node,
+            'get_main_node':        self.resp_main_node,
+            'delete_node':          self.resp_delete_node,
+            'delete_main_node':     self.data,   
+            'add_node':             self.resp_add_node,
+            # 'search_node':          self.search_node,
+            # 'update_node':          self.update_node,
             'heart_beat':           self.resp_heart_beat,
             'set_main_node':        self.resp_set_main_node
         }
-        resp.get(data['op'])(data, reader, writer)        
+        print('gonna request to normal server: ',data['op'])
+        if data['op'] and resp.get(data['op']):
+            await resp.get(data['op'])(data, reader, writer) 
+        else:
+            print('request fail to normal server: ',data['op'])       
         pass
     
 
@@ -195,6 +208,7 @@ class Node:
             toips = [toips]
         if type(data) is not type(''):
             data = json.dumps(data)
+        data = data.encode()
         for ip in toips:                        
             self.client.handler(ip)
             self.client.writer.write(data)
@@ -210,9 +224,10 @@ class Node:
         if not url:
             url = self.mainnode
 
-        self.client.handler(url)
+        await self.client.handler(url)
         if type(data) is not type(''):
             data = json.dumps(data)
+        data = data.encode()
         self.client.writer.write(data)
         await self.client.writer.drain()
         self.client.writer.write_eof()
@@ -227,9 +242,9 @@ class Node:
         """
         
         if self.ismainnode:
-            self.main_node_server(reader, writer)
+            await self.main_node_server(reader, writer)
         else:
-            self.node_server(reader, writer)    
+            await self.node_server(reader, writer)    
 
         await writer.drain()
         writer.write_eof()        
@@ -245,8 +260,8 @@ class Node:
             data = json.dumps(data)
 
         print(f"Received {data!r} from {addr!r}")
-        print(f"Send: {data!r}")
-        writer.write(data)
+        print(f"Send: {data!r}")        
+        writer.write(data.encode())
         
 
     def set_node(self, data):
@@ -359,13 +374,22 @@ class Node:
                     'node': data
                 }, self.childs)
 
+    async def resp_set_main_node(self, data, reader, writer):
+        util.opush(self.scores, data['ip'])
+        if self.con_ip == data['ip']:
+            self.ismainnode = True
+            self.childs = []
+        if self.ismainnode and self.con_ip != data['ip']:
+            await self.notify({'op':'set_main_node', 'ip': data['ip']},self.childs)
+
+
     async def resp_heart_beat(self, data, reader, writer):
         await self.send('1', reader, writer)
 
-    async def resp_delete_main_node(self, data, reader, writer):        
+    async def data(self, data, reader, writer):        
         self.scores.remove(data['node']['ip'])
         self.unset_node(data)
-        if self.ip == self.scores[0]:
+        if self.con_ip == self.scores[0]:
             keys = list(self.resource_mains.keys())
             random.shuffle(keys)
             if len(keys) >5000:
@@ -382,6 +406,9 @@ class Node:
             # reselect the 100s
         if self.ismainnode:
             await self.notify({'op': ' delete_node', 'node': data}, self.childs)
+
+    async def resp_delete_node(self, data, reader, writer):
+        self.unset_node(data)
 
 
     
@@ -416,4 +443,5 @@ class Node:
                     self.scores = await self.requests({'op':'get_main_node'}, url)
                     mainnode =self.choose_best_mainnode()
                     self.node['father'] = mainnode
-                    self.mainnode = mainnode                    
+                    self.mainnode = mainnode  
+                      
--- connector/shell.py
+++ connector/shell.py
@@ -5,6 +5,7 @@ command line relate operation
 import sys
 import asyncio
 from node import Node
+import json
 
 if sys.platform == 'win32':
     loop = asyncio.ProactorEventLoop()
@@ -18,15 +19,15 @@ port = 0
 with open('./config/server.json', encoding = 'utf8') as f:
     serverConfig = json.loads(f.read())
     ip = serverConfig.get('ip')
-    port = serverConfig.get('port')
+    port = serverConfig.get('con_port')
 
 async def main():
     node = Node()
     server = await asyncio.start_server(
-        node.server, ip, port)
-
+        node.server, ip, port)    
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
+    await node.connect_2_network('127.0.0.1:1326')
 
     async with server:
         await server.serve_forever()
